8782c204c96ab73cbde80e8a222be62550f6f5ed
manu-yatnam <manu@yatnam.co.in>
2020-06-10 19:00:57 +0530

    skin

---
+++ skin/adminhtml/default/default/sagepaysuite/js/sagePaySuite_CreateOrder.js
@@ -0,0 +1,131 @@
+repeatpaymentSearchGrid = function () {
+
+    new Control.Modal(
+        $('sagepayrepeat-search-url').getValue(),{
+        className: 'modal-sagepaysuite',
+        iframe: true,
+        height: '500',
+        width: '950',
+        fade: true
+        }
+    ).open();
+
+}
+
+if(typeof EbizmartsSagePaySuite == 'undefined') {
+    var EbizmartsSagePaySuite = {};
+}
+
+EbizmartsSagePaySuite.AdminOrder = Class.create();
+EbizmartsSagePaySuite.AdminOrder.prototype = {
+    initialize: function (config) {
+        this.config             = config;
+        this.servercode            = 'sagepayserver_moto';
+        this.directcode            = 'sagepaydirectpro_moto';
+        this.oldUrl             = '';
+
+        Event.observe(
+            window, 'load', function () {
+            
+            this.oldUrl = $(editForm.formId).readAttribute('action');
+            this.setEditFormAction(SuiteConfig.getConfig('global', 'adminhtml_save_order_url'));
+
+            document.body.insert(
+                new Element(
+                    'a', {
+                    'id': 'sagepayserver-dummy-link',
+                    'href': '#',
+                    'style':'display:none'
+                    }
+                ).update('&nbsp;')
+            );
+            }.bind(this)
+        );
+
+    },
+    evalTransport: function (transport) {
+        try {
+            response = eval('('+transport.responseText+')')
+        } catch(e) {
+            response = {}
+        }
+
+    return response;
+},
+getInstance: function (instance) {
+    return this.config[instance];
+},
+getPaymentMethod: function () {
+    var elements     = $$("#order-billing_method_form [name='payment[method]']");
+    for(var i=0; i<elements.length; i++) {
+        if(elements[i].checked) {
+            return elements[i].value;
+        }
+    }
+
+    return null;
+},
+isServerPaymentMethod: function () {
+    return (this.getPaymentMethod() === this.servercode);
+},
+isDirectPaymentMethod: function () {
+    return (this.getPaymentMethod() === this.directcode);
+},
+setEditFormAction: function (myUrl) {
+    $(editForm.formId).writeAttribute('action', myUrl);
+},
+orderSave: function () {
+
+    //We only need to show iframe for SERVER
+    if(!this.isServerPaymentMethod()) {
+        if(this.isDirectPaymentMethod()) {
+            this.setEditFormAction(SuiteConfig.getConfig('global', 'adminhtml_save_order_url'));
+        }
+        else {
+            this.setEditFormAction(this.oldUrl);
+        }
+        
+        order.submit();
+        return;
+    }
+    
+    this.setEditFormAction(SuiteConfig.getConfig('global', 'adminhtml_save_order_url'));
+
+    //Validate form when SERVER protocol is used
+    if(window.editForm.validator && window.editForm.validate()) {
+        var post_server_fields = $('edit_form').select('input', 'select', 'textarea');
+        new Ajax.Request(
+            SuiteConfig.getConfig('server','sgps_admin_registertrn_url'),{
+            method:"post",
+            parameters: $H(Form.serializeElements(post_server_fields, true)),
+            onSuccess:function (transport) {
+
+                var response = this.evalTransport(transport);
+                if((typeof response.next_url) == 'undefined' || response.response_status == "ERROR"){
+                    alert(response.response_status_detail);
+                    return;
+                }
+
+                $('sagepayserver-dummy-link').writeAttribute('href', response.next_url);
+                new Control.Modal(
+                    'sagepayserver-dummy-link',{
+                    className: 'modal-sagepaysuite sagepaymodal',
+                    closeOnClick: false,
+                    iframe: true,
+                    height: SuiteConfig.getConfig('server','iframe_height'),
+                    width: SuiteConfig.getConfig('server','iframe_width'),
+                    fade: true
+                    }
+                ).open();
+
+            }.bind(this)
+            }
+        );
+    }
+    else {
+        return false;
+    }
+
+}
+
+}
---
+++ skin/frontend/base/default/sagepaysuite/sagePaySuite.js
@@ -0,0 +1,317 @@
+//"use strict";
+
+var getSgpsAjaxUrl = function (method) {
+
+    var loc = window.location.href;
+
+    loc = loc.replace('card/index/','card/');
+    loc = loc.replace('card/index','card/');
+
+    if(loc.slice(-1)== '/'){
+        return loc+method;
+    }else{
+        return loc+'/'+method;
+    }
+};
+
+var tokenSetDefault = function (radioelement) {
+
+    new Ajax.Request(
+        getSgpsAjaxUrl('default'), {
+        method: 'get',
+        parameters: {
+            card:radioelement.value
+        },
+        onSuccess: function (transport) {
+
+            var rsp = transport.responseText.evalJSON();
+
+            if(rsp.st != 'ok'){
+                alert(rsp.text);
+            }
+
+            $('sageTokenCardLoading').hide();
+
+        },
+        onLoading: function () {
+            $('sageTokenCardLoading').show();
+        }
+        }
+    )
+
+};
+
+var evenOdd = function (row, index) {
+    var _class = ((index+1)%2 == 0 ? 'even' : 'odd');
+    row.addClassName(_class);
+};
+
+var updateEvenOdd = function () {
+    var rows = $$('table#my-sagepaycards-table tbody tr');
+    rows.invoke('removeClassName', 'odd').invoke('removeClassName', 'even');
+    rows.each(
+        function (row, index) {
+            evenOdd(row, index);
+        }
+    );
+};
+
+var postRegisterCard = function (frm) {
+
+    var safeForm = new Validation(frm);
+    if(!safeForm.validate()){
+        return;
+    }
+
+    frm.request(
+        {
+        onComplete: function (trn) {
+
+            var rsp = trn.responseText.evalJSON();
+            if(rsp.status != 'OK'){
+                alert(rsp.status.toString() +' -> '+ rsp.statusdetail.toString());
+            }else{
+                if(rsp.mark.ccnickname == ''){
+                    rsp.mark.ccnickname = 'Click to add a Description';
+                }
+
+                var newCardRow = new Template('<tr><td>#{cctype}</td><td class="nickname" id="nickname_#{id}"><div style="cursor: pointer" title="Click to edit the credit card description.">#{ccnickname}</div></td><td>#{ccnumber}</td><td>#{exp}</td><td class="a-center"><input#{defaultchecked} type="radio" value="#{id}" name="tokencard_def" onclick="tokenSetDefault(this);" /></td><td class="last"><a href="#{delurl}" onclick="if(confirm(\''+Translator.translate('Are you sure?')+'\')){removeCard(this); return false; }else{return false;}">'+Translator.translate('Delete')+'</a></td></tr>');
+                new Insertion.Before($$('table#my-sagepaycards-table tbody tr')[0], newCardRow.evaluate(rsp.mark));
+                setObservers();
+                if($('no-tokencards-tr')){
+                    $('no-tokencards-tr').up().remove();
+                }
+
+                $('frmRegCard').remove();
+
+                updateEvenOdd();
+                //window.location.reload();
+            }
+
+            $('sageTokenCardLoading').hide();
+
+        },
+        onLoading: function () {
+            $('sageTokenCardLoading').show();
+        }
+        }
+    )
+
+};
+
+var updateNickname = function (card,nickname,successCallback,failureCallback) {
+
+    var url = getSgpsAjaxUrl('updatenickname/card/'+parseInt(card));
+    new Ajax.Request(
+        url, {
+        method: 'post', //should be patch
+        postBody: 'nickname='+nickname,
+        onSuccess: function (result) {
+
+            if(JSON.parse(result.transport.response).st == 'ok'){
+                if(typeof successCallback == 'function'){
+                    successCallback();
+                }
+
+                $('sageTokenCardLoading').hide();
+            }
+
+        },
+        onFailure: function () {
+            if(typeof failureCallback == 'function'){
+                failureCallback();
+            }
+
+            $('sageTokenCardLoading').hide();
+        },
+        onLoading: function () {
+            $('sageTokenCardLoading').show();
+        }
+        }
+    );
+
+};
+
+var removeCard = function (elem) {
+
+    var oncheckout = elem.hasClassName('oncheckout');
+
+    new Ajax.Request(
+        elem.href, {
+        method: 'get',
+        onSuccess: function (transport) {
+            try {
+                var rsp = transport.responseText.evalJSON();
+
+                if(rsp.st != 'ok') {
+                    new Effect.Opacity(elem.up(), { from: 0.3, to: 1.0, duration: 0.5 });
+                    alert(rsp.text);
+                }
+                else {
+                    if(false === oncheckout) {
+                        elem.up().up().fade(
+                            {
+                            afterFinish:function () {
+                                elem.up().up().remove();
+                                updateEvenOdd();
+                            }
+                            }
+                        );
+                    }
+                    else {
+                        elem.up().fade(
+                            {
+                            afterFinish:function () {
+
+                                var daiv = elem.up('div');
+
+                                elem.up().remove();
+
+                                //If no tokens, open new token dialog
+                                var tokens = daiv.select("li.tokencard-radio input").length;
+                                if(parseInt(tokens) === 0) {
+                                    toggleNewCard(2);
+                                    $$("a.usexist").first().up().remove();
+                                }
+
+                            }
+                            }
+                        );
+                    }
+                }
+
+                if(!oncheckout) {
+                    $('sageTokenCardLoading').hide();
+                }
+            }catch(er){
+                alert(er);
+            }
+        },
+        onLoading: function () {
+            if(!oncheckout) {
+                if($('iframeRegCard')) {
+                    $('iframeRegCard').remove();
+                }
+                else if($('frmRegCard')) {
+                    $('frmRegCard').remove();
+                }
+
+                $('sageTokenCardLoading').show();
+            }
+            else {
+                new Effect.Opacity(elem.up(), { from: 1.0, to: 0.3, duration: 0.5 });
+            }
+
+        }
+        }
+    )
+
+};
+
+var registerCard = function (url) {
+    new Ajax.Request(
+        url, {
+        method: 'get',
+        onSuccess: function (transport) {
+
+            var rsp = transport.responseText.evalJSON();
+
+            if(rsp.url == 'ERROR'){
+                alert(rsp.text);
+            }else{
+                if(rsp.text == 'full_redirect') {
+                    setLocation(rsp.url);
+                }else {
+                    new Insertion.After('link-regcard', rsp.text);
+                }
+            }
+
+            $('sageTokenCardLoading').hide();
+
+        },
+        onLoading: function () {
+
+            if($('iframeRegCard')){
+                $('iframeRegCard').remove();
+            }else if($('frmRegCard')){
+                $('frmRegCard').remove();
+            }
+
+            $('sageTokenCardLoading').show();
+
+        }
+        }
+    )
+}
+
+function trim(str) 
+{
+    return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+}
+
+var editNickname = function (event) {
+
+    this.div = event.findElement();
+    this.td = this.div.up('td');
+    this.textField = '<input type="text" value="'+trim(this.div.innerHTML == 'Click to add a Description' ? '': this.div.innerHTML)+'"/>';
+    this.td.update(this.textField);
+    this.input = this.td.childElements()[0];
+
+    this.div.stopObserving('click', editNickname);
+    this.input.focus();
+    this.input.observe('blur', saveNickname);
+    this.input.observe('keydown', keyPressHandler);
+
+}
+
+var keyPressHandler = function (event) {
+    if (event.keyCode == 13) {
+        saveNickname(event);
+    }
+};
+
+var saveNickname = function (event) {
+
+    this.input = event.findElement();
+    this.td = this.input.up('td');
+
+    this.input.stopObserving('blur', saveNickname);
+
+    this.nickname = this.input.value;
+    this.id = this.input.up('td').id;
+    this.id = parseInt(this.id.replace('nickname_',''));
+
+    var self = this;
+
+    this.successCallback = function () {
+
+        if(self.nickname) {
+            self.td.update('<div style="cursor: pointer" title="Click to edit the credit card description.">' + self.nickname + '</div>');
+        }else {
+            self.td.update('<div style="cursor: pointer" title="Click to edit the credit card description.">Click to add a Description</div>');
+        }
+
+        self.td.down('div').observe('click', editNickname);
+    };
+
+    this.failureCallback = function () {
+        alert('Something went wrong, credit card description not changed.');
+    };
+
+    updateNickname(this.id,this.nickname,this.successCallback,this.failureCallback);
+};
+
+var setObservers = function () {
+    $$('.nickname').each(
+        function (elem) {
+        Event.stopObserving(elem.down('div'), 'click', editNickname);
+        Event.observe(elem.down('div'), 'click', editNickname);
+        }
+    );
+};
+
+document.observe(
+    "dom:loaded", function () {
+    setObservers();
+    }
+);
